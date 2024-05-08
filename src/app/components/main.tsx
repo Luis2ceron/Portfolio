38bf0ad52e87431ce6c802280c53eb050ecff191
= <luiscerontrabajos@gmail.com>
2024-05-08 09:55:51 -0500

    Corrección de la corrección

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -4,7 +4,6 @@ import '../../i18n.ts';
 import './assets/styles/index.css';
 import App from '../App.tsx';
 import { useTranslation } from 'react-i18next';
-import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
