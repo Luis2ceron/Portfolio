5c4393aabb71d9157529475fcb0663c36a1d22cc
luis2ceron <luiscerontrabajos@gmail.com>
2024-05-13 18:04:50 -0500

    Añadir multilenguaje

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
+
+const Footer = () => {
+  const { t } = useTranslation();
+  return (
+    <footer className='flex flex-row items-center justify-between p-4 bg-app-white'>
+      <p>{t('hi')}</p>
+
+    
+    </footer>
+  );
+};
+export default Footer;
