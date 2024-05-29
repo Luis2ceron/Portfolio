20d5da5e16f6b624e03f85e782b70081b28f3d16
luis2ceron <luiscerontrabajos@gmail.com>
2024-05-29 10:16:52 -0500

    sis

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Header from './components/header';
 import HomePage from './screens/homePage';
 
 
@@ -11,7 +10,6 @@ function App() {
     <main className='flex flex-row w-screen h-screen bg-app-white font-roboto'>
     
       <section className='flex-1 grid grid-rows-[auto_1fr_auto]'>
-        <Header />
 
         
         <section className='bg' style={{background : '#1f1f1f'}}>
